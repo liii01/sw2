69b4cc593fac90b3729d7bbe69cd66ffd7f8cb4c
liii01 <117332492+liii01@users.noreply.github.com>
2025-09-20 17:30:17 +0900

    Update 05_practice_2.ino
    2

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,1 +1,22 @@
+#define LED_PIN 7
 
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+  }
+
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  while (1) {}
+}
